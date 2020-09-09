218f720a34be9d4a82e354a8793d38c95993e33c
zhouyunquan <zhouyunquan@dm-maker.com>
2020-09-09 10:31:35 +0800

    init framework

---
+++ Frameworks/DMLinkSDK.framework/Headers/DMLinkSDK.h
@@ -0,0 +1,21 @@
+//
+//  DMLinkSDK.h
+//  DMLinkSDK
+//
+//  Created by Albert on 2020/9/9.
+//  Copyright © 2020 Coder_zzz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <DMLinkSDK/DMLink.h>
+
+
+//! Project version number for DMLinkSDK.
+FOUNDATION_EXPORT double DMLinkSDKVersionNumber;
+
+//! Project version string for DMLinkSDK.
+FOUNDATION_EXPORT const unsigned char DMLinkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMLinkSDK/PublicHeader.h>
+
+
